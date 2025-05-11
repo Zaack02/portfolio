ae2afe32619687f07784fccde00f01e88fe19b61
Zaack02 <isac.lima@sempreceub.com>
2025-05-11 20:19:17 -0300

    Create script.js

---
+++ academicos/academicos/projeto-js-inicial/script.js
@@ -0,0 +1,8 @@
+// projeto-js-inicial
+// Autor: Isac Braga Lima
+
+function saudacao(nome) {
+  return `Olá, ${nome}! Seja bem-vindo ao meu repositório de portfólio.`;
+}
+
+console.log(saudacao("Visitante"));
