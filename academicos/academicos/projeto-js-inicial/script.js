1b9d46f3c9e1dedfef959739741ed937c53a1aff
Zaack02 <isac.lima@sempreceub.com>
2025-05-11 21:08:19 -0300

    docs: adiciona comentários explicativos ao script

--- academicos/academicos/projeto-js-inicial/script.js
+++ academicos/academicos/projeto-js-inicial/script.js
@@ -1,8 +1,16 @@
 // projeto-js-inicial
 // Autor: Isac Braga Lima
+// Este script imprime uma saudação personalizada no console
 
+/**
+ * Função que retorna uma saudação em português.
+ * @param {string} nome - Nome da pessoa a ser saudada.
+ * @returns {string} Mensagem de saudação.
+ */
 function saudacao(nome) {
   return `Olá, ${nome}! Seja bem-vindo ao meu repositório de portfólio.`;
 }
 
+// Executa a saudação com o nome "Visitante"
 console.log(saudacao("Visitante"));
+
